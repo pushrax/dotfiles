357a31a40790c451986466cb6bb76d41d08064f7
Justin Li <jli.justinli@gmail.com>
2013-04-28 16:43:39 -0400

    Remove tmux alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,5 +1,4 @@
 alias redshiftd="redshift > /dev/null &; disown"
 alias mplayer="mplayer -alang jpn -slang eng -ao jack -vo vdpau"
 alias fixtime="sudo sh -c 'ntpd -qg && hwclock -w'"
-alias tmux="TERM=screen-256color-bce tmux"
 alias mp="ncmpcpp"
