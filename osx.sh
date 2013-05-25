e3cf7a3c1fee0d4c4e50e32de5e58d098f400091
Justin Li <jli.justinli@gmail.com>
2013-05-25 02:16:52 -0400

    More OS X stupid tweaks

--- osx.sh
+++ osx.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 # Apple tweaks
 
-# Kill the notification center in Mountain Lion
-launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist
-
 # Less fat font rendering
 defaults -currentHost write -globalDomain AppleFontSmoothing -int 1
+
+# Allow typing things like "fffffffuuuuuuuuuuuu" much more easily...
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Usable key repeat settings
+defaults write -g InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 0
+
+# Kill the notification center in Mountain Lion
+launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist
 
