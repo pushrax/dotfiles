a819ef1bf99026c71012c9af0fd2f3e0f5bc9ed5
Justin Li <jli.justinli@gmail.com>
2013-06-23 21:42:54 -0400

    Add le and mru aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,5 +4,7 @@ alias fixtime="sudo sh -c 'ntpd -qg && hwclock -w'"
 alias mp="ncmpcpp"
 alias bx="bundle exec"
 alias l="ls -alh"
+alias le="less"
+alias mru="ls -ltr"
 
 alias strip-underscores='for i in ./*; do mv -v -- "$i" "${i//_/ }"; done'
