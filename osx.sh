8f68c112096e6bfdd3c2e36993431448b865aa06
Justin Li <jli.justinli@gmail.com>
2014-02-20 20:24:09 -0500

    Disable elastic scroll effects and tighten up animations

--- osx.sh
+++ osx.sh
@@ -1,28 +1,33 @@
 #!/bin/sh
 # Apple tweaks
 
-# Allow typing things like "fffffffuuuuuuuuuuuu" much more easily...
-defaults write -g ApplePressAndHoldEnabled -bool false
-
 # Usable key repeat settings
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 3
 
+# Allow typing things like "fffffffuuuuuuuuuuuu" much more easily...
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Turn off elastic scrolling
+defaults write -g NSScrollViewRubberbanding -int 0
+
+# Don't save to iCloud by default
+defaults write -g NSDocumentSaveNewDocumentsToCloud -bool false
+
 # Kill the notification center in Mountain Lion
 launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist
 
-# Animation speeds
+# Very fast animations
 defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.6
 defaults write com.apple.dock expose-animation-duration -float 0
-
-killall Dock
+defaults write -g NSWindowResizeTime -float 0.2
+defaults write -g NSToolbarFullScreenAnimationDuration -float 0.2
 
 # Show unsigned time machine volumes
 defaults write com.apple.systempreferences TMShowUnsupportedNetworkVolumes 1
 
+killall Dock
+
 # Disable boot sound
 sudo nvram SystemAudioVolume=" "
-
-# Don't save to iCloud by default
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-
