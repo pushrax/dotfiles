b04de5fab2d4516e8ff0b2171dca91a73165f485
Justin Li <jli.justinli@gmail.com>
2014-03-11 23:32:40 -0400

    osx: Enable tap-to-click globally

--- osx.sh
+++ osx.sh
@@ -2,17 +2,17 @@
 # Apple tweaks
 
 # Usable key repeat settings
-defaults write -g InitialKeyRepeat -int 15
-defaults write -g KeyRepeat -int 3
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain KeyRepeat -int 3
 
 # Allow typing things like "fffffffuuuuuuuuuuuu" much more easily...
-defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Turn off elastic scrolling
-defaults write -g NSScrollViewRubberbanding -int 0
+defaults write NSGlobalDomain NSScrollViewRubberbanding -int 0
 
 # Don't save to iCloud by default
-defaults write -g NSDocumentSaveNewDocumentsToCloud -bool false
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
 # Kill the notification center in Mountain Lion
 launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist
@@ -21,8 +21,8 @@ launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0.6
 defaults write com.apple.dock expose-animation-duration -float 0
-defaults write -g NSWindowResizeTime -float 0.2
-defaults write -g NSToolbarFullScreenAnimationDuration -float 0.2
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.2
+defaults write NSGlobalDomain NSToolbarFullScreenAnimationDuration -float 0.2
 
 # Show unsigned time machine volumes
 defaults write com.apple.systempreferences TMShowUnsupportedNetworkVolumes 1
@@ -31,3 +31,8 @@ killall Dock
 
 # Disable boot sound
 sudo nvram SystemAudioVolume=" "
+
+# Turn on tap-to-click globally
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
